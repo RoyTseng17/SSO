391a7a2160033b28c447f4c6eb20758442ce31ff
Roy Tseng <56113903+Tseng17@users.noreply.github.com>
2020-12-18 15:00:19 +0800

    update step wise function matation data

---
+++ main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 14:32:47 2020
+
+@author: Roy Tseng
+"""
+
+from SSO.SSO import SSO
+import random
+
+
+#改寫初始解產生方式
+def create_particle(num):
+    return [random.randint(0,5) for _ in range(data['Nvar'])]
+
+#改寫適應度函數
+def cal_fit(solution, data):
+    total = 0
+    for i in range(data['Nvar']):    
+        total+= solution[i]
+    return total 
+
+#自行設定資料格式
+data = {'Nvar':10} 
+#SSO(data, 母群體個數, 世代數)
+sso = SSO(data, 5,100)
+#替代原本function
+sso.create_particle = create_particle
+sso.cal_fit = cal_fit
+sso.run()
+
+        
+    
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+    name = 'SSO',
+    packages = ['SSO','Algorithm'],
+    scripts = [],
+    version = '0.0.2.R4',
+    description = 'Simplified Swarm Optimization',
+    long_description = "Documentation https://github.com/RoyTseng17/SSO/blob/main/README.md",
+
+    author = 'Roy Tseng',
+    author_email = 'stu121645162002@gmail.com',
+    url = 'https://github.com/RoyTseng17/SSO',
+    download_url = 'https://github.com/RoyTseng17/SSO.git',
+    keywords = ['pypi'],
+    classifiers = []
+)
